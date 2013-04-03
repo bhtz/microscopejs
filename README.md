8629af598cb3d0a089155e4c8bc3bd30f7f24e77
bhtz <bhtz@bhtz-ubuntu.(none)>
2013-04-03 10:57:20 +0200

    updating framework commands and readme

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 microscopejs
 ============
 
-All documentation comming soon !
+install nodejs
+sudo apt-get install nodejs or see http://nodejs.org/
+
+git clone https://github.com/bhtz/microscopejs.git
+or download and unzip microscopejs folder.
+
+cd microscopejs/
+
+try the following commands :
+
+node microscope.js docs
+
+enjoy microscope framework !
--- microscope.js
+++ microscope.js
@@ -16,25 +16,35 @@ var serviceGenerator = require('./lib/serviceGenerator')();
 // shell listener
 // ==========================================
 
+/*
+* some comment here.
+*/
 program
    .version('0.9.0')
    .option('-m, --mobile', 'add mobile views with jQuery mobile from model')
-   .option('-a, --api', 'add restfull web API from model');
+   .option('-a, --api', 'add restfull web API from model')
+   .option('-h, --help', outputDocs());
 
-//
+/*
+* some comment here.
+*/
 program.on('generate_model', function (args) {
     var model = getModel(args);
     modelGenerator.generateModel(model);
 });
 
-//
+/*
+* Generate controller.
+*/
 program.on('generate_controller', function (args) {
     var model = getModel(args);
     controllerGenerator.generateController(model);
     console.log("controller created".green);
 });
 
-//
+/*
+* Generate crud operations.
+*/
 program.on('generate_crud', function (args) {
     var model = getModel(args);
 
@@ -58,27 +68,49 @@ program.on('generate_crud', function (args) {
     if (program.api) { apiControllerGenerator.generateApiController(model); }
 });
 
+/*
+* Generate Restfull web api with model.
+*/
+program.on('generate_api', function(args){
+    var model = getModel(args);
+    modelGenerator.generateModel(model);
+    serviceGenerator.generateService(model);
+    apiControllerGenerator.generateApiController(model);
+});
 
-// synchronize DbContext with database;
+/*
+* synchronize DbContext with database.
+*/
 program.on('db_sync', function(){
     var dbContext = require('./models/dbContext')();
     dbContext.sync();
 });
 
-// drop database tables;
+/*
+* drop database tables.
+*/
 program.on('db_drop', function(){
     var dbContext = require('./models/dbContext')();
     dbContext.drop();
 });
 
-
-// microscope framework generators docs
+/*
+ * microscope framework generators docs.
+ */
 program.on('docs', function () {
-    console.log('\nUse microscope generators with following commands : \n'.yellow);
     console.log('(Type microscope.js --help for command options) \n'.yellow);
+    outputDocs();
+});
+
+// functions
+// ==========================================
+
+function outputDocs(){
+    console.log('\nUse microscope generators with following commands : \n'.yellow);
     var Table = require('cli-table');
     var table = new Table();
     table.push(
+        ['microscope commands'.red, ''],
         ['generate_model'.cyan, 'Generate model class'],
         ['generate_controller'.cyan, 'Generate controller from model'],
         ['generate_crud'.cyan, 'Generate controller, service and views from model'],
@@ -86,22 +118,21 @@ program.on('docs', function () {
         ['db_drop'.cyan, 'Drop your database']
     );
 
+    var optionTable = new Table();
+    optionTable.push(
+        ['generate_crud options'.red, ''],
+        ['-m, --mobile'.cyan, 'Generate jQuery mobile views from model'],
+        ['-a, --api'.cyan, 'Generate restfull web api']
+    );
+
     console.log(table.toString());
-});
+    console.log('\n');
+    console.log(optionTable.toString());
+};
 
-// functions
-// ==========================================
-
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] === obj) {
-            return true;
-        }
-    }
-    return false;
-}
-
+/*
+* Check validity of property type.
+*/
 function isValidPropertyType(type){
     var validateType = ['STRING', 'TEXT', 'DATE', 'BOOLEAN', 'INTEGER', 'FLOAT'];
     if(validateType.contains(type)){
@@ -111,23 +142,26 @@ function isValidPropertyType(type){
     }
 };
 
+/*
+* Get model from arguments.
+*/
 function getModel(args){
     var model = { name: args[0], properties: [] };
     var isValidateModel = true;
 
-	for (var i = 1; i < args.length; i++) {
-		var property = args[i].split(':');
+    for (var i = 1; i < args.length; i++) {
+        var property = args[i].split(':');
         var name = property[0];
         var type = property[1].toUpperCase();
 
         if(isValidPropertyType(type)){
             model.properties.push({ name: name, type: type });    
         }
-		else{
+        else{
             isValidateModel = false;
             console.log('\nunknown type for property : '.red + name);
         }
-	}
+    }
 
     if(isValidateModel)
     {
@@ -138,7 +172,23 @@ function getModel(args){
     }
 }
 
+/*
+* Extend JavaScript Array prototype.
+*/
+Array.prototype.contains = function(obj) {
+    var i = this.length;
+    while (i--) {
+        if (this[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
 // execute
 // ==========================================
 
+/*
+* Parse shell commands.
+*/
 program.parse(process.argv);
