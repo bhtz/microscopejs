2a2b22051964a12c882d29682b56047305837f9f
bhtz <benjamin.heintz@live.com>
2013-09-14 02:33:09 +0200

    updating package.json

--- bin/microscope.js
+++ bin/microscope.js
@@ -1,3 +1,4 @@
-var CommandLineInterface = require("../lib/commandLineInterface");
+#!/usr/bin/env node
+var CommandLineInterface = require(__dirname +'/../lib/commandLineInterface');
 var commandLineInterface = new CommandLineInterface();
 commandLineInterface.display_microscope_form();
