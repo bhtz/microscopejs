4819977af328d089b8b0ebffacdfd451bd25608f
bhtz <benjamin.heintz@live.com>
2013-09-16 19:00:11 +0200

    updating package.json and bin

--- bin/microscope.js
+++ bin/microscope.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
-var CommandLineInterface = require('../lib/commandLineInterface');
+var path = require('path');
+var fs   = require('fs');
+var libPath  = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
+
+var CommandLineInterface = require(libPath + '/commandLineInterface');
 var commandLineInterface = new CommandLineInterface();
 commandLineInterface.display_microscope_form();
